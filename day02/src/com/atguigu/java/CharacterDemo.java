ea403f6ccab6b080b2e2ccaf9f79520ccfc4412b
liyuan1978 <liyuanheluosheng333@hotmail.com>
2022-05-15 00:36:10 +0900

    添加day02

---
+++ day02/src/com/atguigu/java/CharacterDemo.java
@@ -0,0 +1,14 @@
+package com.atguigu.java;
+
+/**
+ * @author liyuan_start
+ * @create 2022-02-05 18:50
+ */
+
+
+
+class CharacterDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World!你好，中国！");
+    }
+}
