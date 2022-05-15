fe1cbdff1faf22f43944c068e56ef869f8bbe1f0
liyuan1978 <liyuanheluosheng333@hotmail.com>
2022-05-15 21:36:58 +0900

    day07 排序算法（冒泡，快排）

--- day07/src/com/liyuan/java/QuickSort.java
+++ day07/src/com/liyuan/java/QuickSort.java
@@ -4,8 +4,8 @@ package com.liyuan.java;
  * 快速排序
  * 通过一趟排序将待排序记录分割成独立的两部分，其中一部分记录的关键字均比另一部分关键字小，
  * 则分别对这两部分继续进行排序，直到整个序列有序。
- * @author shkstart
- * 2018-12-17
+ *
+ *
  */
 public class QuickSort {
 	private static void swap(int[] data, int i, int j) {
